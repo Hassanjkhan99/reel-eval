d4ebd858781f7140e370b848ee823c6e0df4dcac
HassanJkhan99 <hassanjamilkhan114w@gmail.com>
2022-10-25 02:38:08 +0500

    AddProspect

---
+++ src/app/modules/prospect/add-prospect/add-prospect.component.ts
@@ -0,0 +1,63 @@
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NzGridModule} from "ng-zorro-antd/grid";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NzFormModule} from "ng-zorro-antd/form";
+import {ProspectService} from "../../../shared/services/prospect.service";
+import {NzInputModule} from "ng-zorro-antd/input";
+import {NzButtonModule} from "ng-zorro-antd/button";
+import {map} from "rxjs/operators";
+import {NzSelectModule} from "ng-zorro-antd/select";
+
+@Component({
+  selector: 'app-add-prospect',
+  standalone: true,
+  imports: [CommonModule, NzGridModule, ReactiveFormsModule, NzFormModule, NzInputModule, NzButtonModule, NzSelectModule, FormsModule],
+  templateUrl: './add-prospect.component.html',
+  styleUrls: ['./add-prospect.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AddProspectComponent implements OnInit {
+  prospectForm: FormGroup;
+  selectedValue = null;
+  positions;
+
+  constructor(private fb: FormBuilder, private prospectService: ProspectService) {
+    this.prospectForm = this.fb.group({
+      first_name: ['', [Validators.required]],
+      last_name: ['', [Validators.required]],
+      position: [0, [Validators.required, Validators.maxLength(2)]],
+      classification: ['', [Validators.required]],
+      state: ['', [Validators.required]],
+      school: ['', [Validators.required]],
+      video_link: [''],
+    });
+  }
+
+  ngOnInit(): void {
+    this.getPositions();
+
+  }
+
+  submitForm(value: {
+    first_name: string; last_name: string; position: number; classification: string; state: string;
+    school: string; video_link: string;
+  }): void {
+    for (const key in this.prospectForm.controls) {
+      this.prospectForm.controls[key].markAsDirty();
+      this.prospectForm.controls[key].updateValueAndValidity();
+    }
+    this.prospectService.postAddProspect(value).subscribe();
+    console.log(value);
+  }
+
+  getPositions() {
+    this.prospectService.getPositions().pipe(map(value => value.results)).subscribe(
+      x => {
+        this.positions = x;
+        console.log(x);
+      }
+    )
+  }
+
+}
--- src/app/shared/services/staff.service.ts
+++ src/app/shared/services/staff.service.ts
@@ -3,7 +3,8 @@ import {HttpClient} from "@angular/common/http";
 import {SignUp} from "../interfaces/authentication.interface";
 import {Observable} from "rxjs";
 import {main_url} from "../../../environments/environment";
-import {Staff} from "../interfaces/staff.interface";
+import {Staff, StaffList} from "../interfaces/staff.interface";
+import {Prospect} from "../interfaces/prospect.interface";
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,9 @@ export class StaffService {
 
   postAddCoach(payload: Staff): Observable<Staff> {
     return this.http.post<Staff>(`${main_url}core/register/`, payload);
+  }
+
+  getStaff(): Observable<StaffList> {
+    return this.http.get<StaffList>(`${main_url}accounts/`);
   }
 }
