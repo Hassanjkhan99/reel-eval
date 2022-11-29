861c2e6fc31d0b2361e13d2c8c49a3231dce90a5
Hassanjkhan99 <hassanjamilkhan114@gmail.com>
2022-11-30 00:45:47 +0500

    weights component

--- src/app/modules/grading/pre-grading/assign-weights/pill-with-input/pill-with-input.component.ts
+++ src/app/modules/grading/pre-grading/assign-weights/pill-with-input/pill-with-input.component.ts
@@ -1,27 +1,35 @@
-import {ChangeDetectorRef, Component, forwardRef, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {ChangeDetectorRef, Component, forwardRef, Input, OnChanges, OnInit, SimpleChanges,} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {NzIconModule} from "ng-zorro-antd/icon";
-import {NzInputModule} from "ng-zorro-antd/input";
-import {NzGridModule} from "ng-zorro-antd/grid";
-import {ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR, ReactiveFormsModule} from "@angular/forms";
-import {Trait} from "../../../../../shared/interfaces/trait";
-import {UntilDestroy, untilDestroyed} from "@ngneat/until-destroy";
+import {NzIconModule} from 'ng-zorro-antd/icon';
+import {NzInputModule} from 'ng-zorro-antd/input';
+import {NzGridModule} from 'ng-zorro-antd/grid';
+import {ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR, ReactiveFormsModule,} from '@angular/forms';
+import {Trait} from '../../../../../shared/interfaces/trait';
+import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
 
 @UntilDestroy()
 @Component({
   selector: 'app-pill-with-input',
   standalone: true,
-  imports: [CommonModule, NzIconModule, NzInputModule, NzGridModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    NzIconModule,
+    NzInputModule,
+    NzGridModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './pill-with-input.component.html',
   styleUrls: ['./pill-with-input.component.scss'],
-  providers: [{
-    provide: NG_VALUE_ACCESSOR,
-    useExisting: forwardRef(() => PillWithInputComponent),
-    multi: true,
-  }],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PillWithInputComponent),
+      multi: true,
+    },
+  ],
 })
-export class PillWithInputComponent implements OnInit, ControlValueAccessor, OnChanges {
-
+export class PillWithInputComponent
+  implements OnInit, ControlValueAccessor, OnChanges {
   @Input() item: Trait;
   @Input() remainingLimit: number;
   control = new FormControl<number>(0);
@@ -36,11 +44,24 @@ export class PillWithInputComponent implements OnInit, ControlValueAccessor, OnC
   }
 
   ngOnInit(): void {
+    this.control.valueChanges.pipe(untilDestroyed(this)).subscribe((value) => {
+      if (value > this.prevValue + this.remainingLimit) {
+        this.control.setValue(this.prevValue, {emitEvent: false});
+      } else {
+        this.prevValue = value;
+      }
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.remainingLimit) {
+      this.prevValue = this.control.value;
+    }
   }
 
   registerOnChange(fn: (val: number) => unknown): void {
     this.control.valueChanges.pipe(untilDestroyed(this)).subscribe((value) => {
-      fn(value)
+      fn(value);
     });
   }
 
@@ -61,5 +82,4 @@ export class PillWithInputComponent implements OnInit, ControlValueAccessor, OnC
       this.control.reset(0);
     }
   }
-
 }
