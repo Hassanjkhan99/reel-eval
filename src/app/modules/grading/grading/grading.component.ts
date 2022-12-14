f779019ed802b2a95f6e1da3f0f02bd2f972776e
Hassanjkhan99 <Hassanjk12345>
2022-12-15 04:37:12 +0500

    grading

--- src/app/modules/grading/grading/grading.component.ts
+++ src/app/modules/grading/grading/grading.component.ts
@@ -1,27 +1,94 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit,} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {NzTableModule} from "ng-zorro-antd/table";
-import {NzIconModule} from "ng-zorro-antd/icon";
-import {FormArray} from "@angular/forms";
+import {NzTableModule} from 'ng-zorro-antd/table';
+import {NzIconModule} from 'ng-zorro-antd/icon';
+import {NzButtonModule} from "ng-zorro-antd/button";
 
 @Component({
   selector: 'app-grading',
   standalone: true,
-  imports: [CommonModule, NzTableModule, NzIconModule],
+  imports: [CommonModule, NzTableModule, NzIconModule, NzButtonModule],
   templateUrl: './grading.component.html',
   styleUrls: ['./grading.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GradingComponent implements OnInit {
   listOfColumns = ['Bend', 'Effort', 'Heavy Hands', 'Pad Level', 'Technique'];
-  grading = new FormArray([])
+  grading = [];
+  columnValue = {};
+  totalValue = 0
 
-  constructor() {
+  constructor(private cdr: ChangeDetectorRef) {
   }
 
   ngOnInit(): void {
-    this.listOfColumns.map(col => {
-    })
+    this.columnValue = this.listOfColumns.reduce(
+      (a, v) => ({...a, [v]: 100}),
+      {}
+    );
+    console.log(this.columnValue);
+    this.addRow();
   }
+
+  addRow() {
+    const columnsWithValue = this.listOfColumns.reduce(
+      (a, v) => ({...a, [v]: 100}),
+      {}
+    );
+    let obj = {...columnsWithValue, rowNumber: this.grading.length + 1};
+
+    this.grading = [...this.grading, obj];
+    this.cdr.detectChanges();
+    console.log(this.grading);
+    this.listOfColumns.forEach((e) => {
+      this.calculateColumn(e);
+    });
+  }
+
+  removeRow(index: number) {
+    if (this.grading.length < 2) {
+      return;
+    }
+    this.grading = this.grading.filter((e, i) => i !== index);
+    this.cdr.detectChanges();
+    console.log(this.grading);
+  }
+
+  calculateColumn(columnName: string) {
+    const value = this.grading.map((e) => {
+      return e[columnName];
+    });
+    const length = value.length;
+    this.columnValue[columnName] =
+      value.reduce((prev, curr, i) => {
+        return prev + curr;
+      }) / length;
+    console.log({value});
+    this.calculateOverAll()
+  }
+
+  calculateOverAll() {
+    let sum = 0
+    for (const key in this.columnValue) {
+      sum += this.columnValue[key]
+    }
+    this.totalValue = sum / this.listOfColumns.length
+  }
+
+  increment(column: string, i: number) {
+    if (this.grading[i][column] > 99) {
+      return;
+    }
+    this.grading[i][column]++;
+    this.calculateColumn(column);
+  }
+
+  decrement(column: string, i: number) {
+    if (this.grading[i][column] < 1) {
+      return;
+    }
+    this.grading[i][column]--;
+    this.calculateColumn(column);
+  }
+
 
 }
