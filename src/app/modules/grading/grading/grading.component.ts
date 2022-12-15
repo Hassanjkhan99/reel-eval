24a941dd90cbbc57a58ecd0ff521fa20f2bd8b57
Hassanjkhan99 <Hassanjamilkhan114@gmail.com>
2022-12-16 04:01:48 +0500

    grading partial

--- src/app/modules/grading/grading/grading.component.ts
+++ src/app/modules/grading/grading/grading.component.ts
@@ -3,11 +3,12 @@ import {CommonModule} from '@angular/common';
 import {NzTableModule} from 'ng-zorro-antd/table';
 import {NzIconModule} from 'ng-zorro-antd/icon';
 import {NzButtonModule} from "ng-zorro-antd/button";
+import {NzToolTipModule} from "ng-zorro-antd/tooltip";
 
 @Component({
   selector: 'app-grading',
   standalone: true,
-  imports: [CommonModule, NzTableModule, NzIconModule, NzButtonModule],
+  imports: [CommonModule, NzTableModule, NzIconModule, NzButtonModule, NzToolTipModule],
   templateUrl: './grading.component.html',
   styleUrls: ['./grading.component.scss'],
 })
