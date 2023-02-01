ddacb887fb1143ad4788fbc6d52881e8edc58607
TalhaJamilTJ <Tjamil@macrosoftinc.com>
2023-02-01 20:52:28 +0500

    environment change

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/reel-eval'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/reel-eval/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname +
+    '/dist/reel-eval/index.html'));
+});
+app.listen(process.env.PORT || 80);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true
 };
+export const main_url = 'https://api.reeleval.com/prod/';
